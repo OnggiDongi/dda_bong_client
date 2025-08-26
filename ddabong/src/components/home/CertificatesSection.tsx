6a7ad44120f5ad2ae637e869ffbd6b6311c3229b
kimkiyeonn <paynjoy2@naver.com>
2025-08-26 14:07:17 +0900

    📌 Feat    : 인증서 컴포넌트, 인증서 구현

---
+++ ddabong/src/components/home/CertificatesSection.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import Txt from '@/components/atoms/Text';
+import CertificateCard from './CertificateCard';
+
+const certificates: {
+  title: string;
+  date: string;
+  totalHours: string;
+  bgColor: string;
+  borderColor?: string;
+}[] = [
+  // 테스트 데이터
+  {
+    title: '시별돌',
+    date: '2026.09.05',
+    totalHours: '50',
+    bgColor: 'bg-1Q-Mint',
+    borderColor: 'border-1Q-Mint-Line',
+  },
+  {
+    title: '시별돌',
+    date: '2026.09.08',
+    totalHours: '100',
+    bgColor: 'bg-1Q-Purple',
+    borderColor: 'border-1Q-Purple-Line',
+  },
+];
+
+export default function CertificatesSection() {
+  return (
+    <section className='h-[280px] w-[350px] rounded-[20px] bg-white shadow-[0_0_5px_0_rgba(0,0,0,0.15)]'>
+      <Txt
+        weight='bold'
+        className='block px-[31px] pt-[25px] pb-[18px] text-left text-xl'
+      >
+        나의 인증서
+      </Txt>
+
+      {certificates.length > 0 ? (
+        <div className='flex justify-center gap-12'>
+          {certificates.map((cert, idx) => (
+            <CertificateCard key={idx} {...cert} />
+          ))}
+        </div>
+      ) : (
+        <div className='flex h-40 items-center justify-center text-center'>
+          <Txt weight='semibold' className='text-Hana-Black text-xl'>
+            봉사 시간을 채워 <br /> 인증서를 받아보세요!
+          </Txt>
+        </div>
+      )}
+    </section>
+  );
+}
