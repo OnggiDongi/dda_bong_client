ce8c23f9c5876ad086f5c701375d133b421d4aec
soobin <subin3620@naver.com>
2025-08-27 10:31:16 +0900

    📌 Feat    : 시니어 회원가입 페이지 재구현

--- ddabong/src/app/signUp/senior/page.tsx
+++ ddabong/src/app/signUp/senior/page.tsx
@@ -1,28 +1,65 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import Button from '@/components/atoms/Button';
 import Input from '@/components/atoms/Input';
 import Txt from '@/components/atoms/Text';
+import DatePicker from '@/components/common/DatePicker';
 
 export default function SeniorSignUpPage() {
+  const [joinDate, setJoinDate] = useState<Date>(new Date());
   return (
-    <main className='bg-Page-Background flex min-h-screen flex-col items-center pt-25'>
+    <main className='bg-Page-Background flex min-h-screen flex-col items-center pt-[54px]'>
       {/* 로고 */}
       <Image
         src='/icons/ic_logo.svg'
         alt='따봉 로고'
-        width={150}
-        height={90}
+        width={100}
+        height={60}
         className='mt-[18px] object-contain'
         priority
       />
 
       {/* 폼 컨테이너 */}
-      <div className='mt-[44px] w-[300px]'>
-        <form className='flex flex-col'>
+      <div className='w-[300px]'>
+        <form className='flex flex-col gap-2'>
+          {/* 이름 */}
+          <label className='block'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
+              이름
+            </Txt>
+            <Input
+              type='text'
+              placeholder='이름을 입력해주세요'
+              required
+              maxLength={50}
+              className='text-Hana-Black placeholder:text-Icon-Detail h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
+              aria-label='이름 입력'
+            />
+          </label>
+
+          {/* 생년월일 */}
+          <label className='block'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
+              생년월일
+            </Txt>
+            <DatePicker value={joinDate} onChange={setJoinDate} />
+          </label>
+
           {/* 이메일 */}
           <label className='block'>
-            <Txt weight='semibold' className='text-Hana-Black text-2xl'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
               이메일
             </Txt>
             <Input
@@ -31,60 +68,89 @@ export default function SeniorSignUpPage() {
               autoComplete='email'
               required
               maxLength={50}
-              className='text-Hana-Black placeholder:text-Icon-Detail mt-[10px] mb-[25px] h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
+              className='text-Hana-Black placeholder:text-Icon-Detail h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
               aria-label='이메일 입력'
             />
           </label>
 
           {/* 비밀번호 */}
           <label className='block'>
-            <Txt weight='semibold' className='text-Hana-Black text-2xl'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
               비밀번호
             </Txt>
             <Input
               type='password'
               placeholder='비밀번호'
-              autoComplete='current-password'
+              autoComplete='new-password'
               required
               maxLength={50}
-              className='text-Hana-Black placeholder:text-Icon-Detail mt-[10px] mb-[45px] h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
+              className='text-Hana-Black placeholder:text-Icon-Detail h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
               aria-label='비밀번호 입력'
             />
           </label>
 
-          {/* 로그인 버튼 */}
-          <Link href='/home' className='mb-[25px] h-[50px] w-full'>
-            <Button className='h-[50px] w-full font-[AppleSDGothicNeoSB] text-[26px]'>
-              로그인
-            </Button>
-          </Link>
-
-          {/* 카카오 버튼 */}
-          <Button className='bg-kakao text-Hana-Black h-[50px] w-full gap-2 font-[AppleSDGothicNeoSB] text-xl'>
-            <Image
-              src='/icons/ic_kakao.svg'
-              alt='카카오 아이콘'
-              width={22}
-              height={22}
-              className='object-contain'
+          {/* 비밀번호 확인 */}
+          <label className='block'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
+              비밀번호 확인
+            </Txt>
+            <Input
+              type='password'
+              placeholder='비밀번호 확인'
+              autoComplete='new-password'
+              required
+              maxLength={50}
+              className='text-Hana-Black placeholder:text-Icon-Detail h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
+              aria-label='비밀번호 확인 입력'
             />
-            카카오톡으로 로그인하기
+          </label>
+
+          {/* 전화번호 */}
+          <label className='block'>
+            <Txt
+              weight='semibold'
+              className='text-Hana-Black mb-2 block text-2xl'
+            >
+              전화번호
+            </Txt>
+            <Input
+              type='tel'
+              placeholder='전화번호'
+              required
+              maxLength={13}
+              className='text-Hana-Black placeholder:text-Icon-Detail h-[50px] w-full pl-5 font-[AppleSDGothicNeoM] text-[26px] placeholder:font-[AppleSDGothicNeoM] placeholder:text-[26px]'
+              aria-label='전화번호 입력'
+            />
+          </label>
+
+          {/* 회원가입 버튼 */}
+          <Button
+            type='submit'
+            className='mt-[30px] h-[50px] w-full font-[AppleSDGothicNeoSB] text-[26px]'
+          >
+            회원가입
           </Button>
 
-          {/* 회원가입으로 이동 */}
-          <div className='mt-[25px] flex items-center justify-center'>
+          {/* 로그인으로 이동 */}
+          <div className='mt-4 flex items-center justify-center'>
             <Txt
               weight='medium'
               className='text-Icon-Detail text-xl leading-none'
             >
-              가입한 계정이 없으신가요?
+              계정이 이미 있으신가요?
             </Txt>
-            <Link href='/signUp/senior' className='mb-1 ml-[14px]'>
+            <Link href='/signIn/senior' className='mb-1 ml-[14px]'>
               <Txt
                 weight='medium'
                 className='text-Icon-Detail align-middle text-xl leading-none underline underline-offset-2'
               >
-                회원가입
+                로그인
               </Txt>
             </Link>
           </div>
