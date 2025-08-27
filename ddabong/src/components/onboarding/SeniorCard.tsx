e6f346ad15f1c587cdfc3702486e213d520ef30a
soobin <subin3620@naver.com>
2025-08-27 13:36:54 +0900

    📌 Feat    : 사용하지않는 props 제거

--- ddabong/src/components/onboarding/SeniorCard.tsx
+++ ddabong/src/components/onboarding/SeniorCard.tsx
@@ -1,17 +1,14 @@
 import BannerCard from '../atoms/BannerCard';
 
-type Props = { className?: string };
-
-export default function SeniorCard({ className }: Props) {
+export default function SeniorCard() {
   return (
     <BannerCard
-      titleTop="봉사를 찾고있는"
-      titleBottom="개인인가요?"
-      iconSrc="/icons/ic_senior.svg"
-      href="/seniorLogin"
-      bgClass="bg-Hana-Beige"
-      borderClass="border-Hana-Beige-Line"
-      className={className}
+      titleTop='봉사를 찾고있는'
+      titleBottom='개인인가요?'
+      iconSrc='/icons/ic_senior.svg'
+      href='/signIn/senior'
+      bgClass='bg-Hana-Beige'
+      borderClass='border-Hana-Beige-Line'
     />
   );
 }
