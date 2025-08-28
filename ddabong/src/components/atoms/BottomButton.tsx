9f6aea96eee62d1d63fd8826fedc1694470f8b77
soobin <subin3620@naver.com>
2025-08-28 10:13:10 +0900

    📌 Feat    : 하단 고정 버튼 컴포넌트 생성

---
+++ ddabong/src/components/atoms/BottomButton.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Button from '@/components/atoms/Button';
+
+interface SubmitButtonProps {
+  label?: string;
+  onClick?: () => void;
+}
+
+export default function BottomButton({
+  label = '작성 완료',
+  onClick,
+}: SubmitButtonProps) {
+  return (
+    <div className='bg-white px-[26px] py-3 shadow-[0_0_5px_0_rgba(0,0,0,0.15)]'>
+      <Button onClick={onClick} className='w-full'>
+        {label}
+      </Button>
+    </div>
+  );
+}
