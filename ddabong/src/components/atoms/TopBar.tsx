81c73db3737401dbeb2e2c8dcadd0963338616c6
soobin <subin3620@naver.com>
2025-08-28 10:12:56 +0900

    📌 Feat    : TopBar 수정

--- ddabong/src/components/atoms/TopBar.tsx
+++ ddabong/src/components/atoms/TopBar.tsx
@@ -1,19 +1,24 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import Txt from '@/components/atoms/Text';
 
 type Props = {
   title?: string;
-  onBack?: () => void;
+  onBack?: () => void; // 외부 핸들러가 있으면 우선 사용
 };
 
 export default function TopBar({ title = '봉사 등록하기', onBack }: Props) {
+  const router = useRouter();
+  const handleBack = () => (onBack ? onBack() : router.back());
+
   return (
     <header className='relative flex items-center justify-center bg-white'>
       <button
         type='button'
-        onClick={onBack}
+        onClick={handleBack}
         className='absolute start-0 ml-[19px] pt-[25px] pb-[22px]'
-        aria-label='뒤로가기'
       >
         <Image
           src='/icons/ic_arrow_back.svg'
