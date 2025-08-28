beee606076c37ad1cfba5a2a6ff56fad216f3f34
bokyeommmmm <bokyeommmmm@github.com>
2025-08-28 13:18:35 +0900

    🎈 Chore   : 모니터 점 제거

--- ddabong/src/components/admin/register/Detail.tsx
+++ ddabong/src/components/admin/register/Detail.tsx
@@ -1,5 +1,4 @@
 import Txt from '@/components/atoms/Text';
-import Textarea from '@/components/atoms/Textarea';
 
 export default function Detail() {
   return (
@@ -8,10 +7,10 @@ export default function Detail() {
         상세 정보
       </Txt>
       <div className='mt-3 px-[26px]'>
-        <Textarea
-          rows={5}
+        <textarea
           placeholder='봉사 활동에 대한 상세 정보를 적어주세요'
-          className='border-Box-Line text-Hana-Black w-full rounded-xl border bg-white py-[11px] pl-[14px] text-base'
+          required
+          className='placeholder:text-Icon-Detail border-Box-Line h-[150px] w-full resize-none overflow-y-auto rounded-xl border bg-white py-[11px] pl-[14px] text-base focus:outline-none'
         />
       </div>
     </div>
