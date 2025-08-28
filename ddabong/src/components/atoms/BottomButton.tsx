acfeeb27108662a77fa765f9605952390732a77f
soobin <subin3620@naver.com>
2025-08-28 10:21:36 +0900

    🎨 Design  : 하단 버튼 위치 수정

--- ddabong/src/components/atoms/BottomButton.tsx
+++ ddabong/src/components/atoms/BottomButton.tsx
@@ -12,7 +12,7 @@ export default function BottomButton({
   onClick,
 }: SubmitButtonProps) {
   return (
-    <div className='bg-white px-[26px] py-3 shadow-[0_0_5px_0_rgba(0,0,0,0.15)]'>
+    <div className='mt-auto bg-white px-[26px] py-3 shadow-[0_0_5px_0_rgba(0,0,0,0.15)]'>
       <Button onClick={onClick} className='w-full'>
         {label}
       </Button>
