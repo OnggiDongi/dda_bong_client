07c682288728db084b24cc7a96cb0433bd3b6be8
kimkiyeonn <paynjoy2@naver.com>
2025-08-29 15:52:29 +0900

    🎨 Design  : Header 통일

--- ddabong/src/components/review/VolunProfile.tsx
+++ ddabong/src/components/review/VolunProfile.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import Txt from '../atoms/Text';
 
-//테스트 데이터
-const UserData = { userName: '시별돌', volunHour: 35 };
-export default function VolunProfile() {
+type Props = {
+  volunHour: number;
+};
+
+export default function VolunProfile({ volunHour }: Props) {
   return (
     <section className='border-Logo-Mint flex h-[130px] w-[350px] items-center justify-center rounded-2xl border bg-white'>
       <div className='flex flex-col justify-center pr-11'>
         <Txt weight='bold' className='text-[22px]'>
-          누적 봉사시간
+          누적 봉사 시간
         </Txt>
         <Txt weight='heavy' className='text-Logo-Mint text-[22px]'>
-          {UserData.volunHour}시간
+          {volunHour}시간
         </Txt>
       </div>
       <Image
