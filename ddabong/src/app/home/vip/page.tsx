e04106103c0190be7a1c7096315c2fc01f712869
bokyeommmmm <bokyeommmmm@github.com>
2025-09-01 11:16:24 +0900

    📌 Feat    : home/vip

--- ddabong/src/app/home/vip/page.tsx
+++ ddabong/src/app/home/vip/page.tsx
@@ -1,3 +1,22 @@
+import TopBar from '@/components/atoms/TopBar';
+import VipBenefitCard from '@/components/home/vip/VipBenefitCard';
+import VipBenefitSection from '@/components/home/vip/VipBenefitSection';
+import VipLevelCard from '@/components/home/vip/VipLevelCard';
+
 export default function VipPage() {
-  return <div>나의 등급</div>;
+  return (
+    <div className='bg-page-gradient flex h-screen w-full flex-col'>
+      <TopBar title='나의 등급' bgColor='bg-page-background' />
+      <div className='flex-1 overflow-y-auto px-[26px] pt-[23px]'>
+        <VipLevelCard userName='시별돌' totalHours={69} />
+        <div className='border-Box-Line border-b pb-[269px]'>
+          <VipBenefitCard
+            benefitText='봉사 50시간마다 새로운 인증서를 발급해드립니다 !'
+            imageSrc='/icons/ic_flyingStar.svg'
+          />
+        </div>
+        <VipBenefitSection />
+      </div>
+    </div>
+  );
 }
---
+++ ddabong/src/components/home/vip/VipBenefitSection.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Txt from '@/components/atoms/Text';
+
+export default function VipBenefitSection() {
+  const benefits = [
+    '금융 전문가 1:1 상담 (가족 간 양도 가능)',
+    '전용 창구/콜센터 (가족 공통 이용 가능)',
+    '대출·부동산 상담 우선권 (가족 공통 이용 가능)',
+    '일반 국가검진에 없는 항목 지원',
+  ];
+
+  return (
+    <section className='pt-[23px]'>
+      <div>
+        <Txt weight='bold' className='pl-[16px] text-2xl'>
+          VIP 등급 혜택
+        </Txt>
+      </div>
+      <Txt className='text-Modal-font pl-[19px] text-lg underline underline-offset-4'>
+        * 최근 6개월 간 누적 봉사 111시간 달성 시
+      </Txt>
+
+      <ul className='list pt-[17px] pl-6'>
+        {benefits.map((benefit, i) => (
+          <li key={i}>
+            <Txt className='text-lg'>{benefit}</Txt>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
