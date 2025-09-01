9a3be44c9353508dccfab275fb3d1115be4c64d6
Ausdauer1 <steady051@naver.com>
2025-09-01 10:35:26 +0900

    📌 Feat    : 기능 (footer에 모달 기능 추가)

--- ddabong/src/components/apply/ApplyFooter.tsx
+++ ddabong/src/components/apply/ApplyFooter.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Button from '../atoms/Button';
+import Modal from '../atoms/modal';
 
 type ApplyFooterProps = {
   isApply: boolean;
 };
 
 export default function ApplyFooter({ isApply = true }: ApplyFooterProps) {
+  const router = useRouter();
   const [liked, setLiked] = useState(false);
+  const [isModalOpened, setModalOpened] = useState<boolean>(false);
+
+  const openModal = () => {
+    setModalOpened(true);
+  };
+  const closeModal = () => setModalOpened(false);
 
   const applyDelete = async () => {
     try {
@@ -30,48 +39,67 @@ export default function ApplyFooter({ isApply = true }: ApplyFooterProps) {
   }, [liked]);
 
   return (
-    <section
-      className={`relative flex h-[68px] items-center justify-center shadow-[0_0_5px_0_rgba(0,0,0,0.15)] ${isApply ? 'gap-32' : 'gap-10'}`}
-    >
-      {isApply ? (
-        <button
-          type='button'
-          onClick={() => setLiked((v) => !v)}
-          aria-pressed={liked}
-          aria-label={liked ? '좋아요 취소' : '좋아요'}
-          className=''
-        >
-          <span className='relative inline-block h-[30px] w-[30px]'>
-            <Image
-              src={
-                liked
-                  ? '/icons/ic_heart_filled.svg'
-                  : '/icons/ic_heart_outline.svg'
-              }
-              alt=''
-              fill
-              className='object-contain'
-            />
-          </span>
-        </button>
-      ) : (
-        <Button
-          className='bg-Background h-[45px] w-[145px]'
-          textClassName='text-Hana-Black/60'
-          onClick={() => alert('수정하기')}
-        >
-          수정하기
-        </Button>
-      )}
-
-      <Button
-        className={`h-[45px] w-[145px] ${
-          isApply ? 'bg-Logo-Mint' : 'bg-Logo-Pink'
-        }`}
-        onClick={applyDelete}
+    <>
+      <section
+        className={`relative flex h-[68px] items-center justify-center shadow-[0_0_5px_0_rgba(0,0,0,0.15)] ${isApply ? 'gap-32' : 'gap-10'}`}
       >
-        {isApply ? '신청하기' : '삭제하기'}
-      </Button>
-    </section>
+        {isApply ? (
+          <button
+            type='button'
+            onClick={() => setLiked((v) => !v)}
+            aria-pressed={liked}
+            aria-label={liked ? '좋아요 취소' : '좋아요'}
+            className=''
+          >
+            <span className='relative inline-block h-[30px] w-[30px]'>
+              <Image
+                src={
+                  liked
+                    ? '/icons/ic_heart_filled.svg'
+                    : '/icons/ic_heart_outline.svg'
+                }
+                alt=''
+                fill
+                className='object-contain'
+              />
+            </span>
+          </button>
+        ) : (
+          <Button
+            className='bg-Background h-[45px] w-[145px]'
+            textClassName='text-Hana-Black/60'
+            onClick={() => router.push('/admin/recruit/write/1')}
+          >
+            수정하기
+          </Button>
+        )}
+
+        <Button
+          className={`h-[45px] w-[145px] ${
+            isApply ? 'bg-Logo-Mint' : 'bg-Logo-Pink'
+          }`}
+          onClick={openModal}
+        >
+          {isApply ? '신청하기' : '삭제하기'}
+        </Button>
+      </section>
+      {isModalOpened && (
+        <Modal
+          title='동해 바다 쓰레기 줍기'
+          description={
+            isApply
+              ? '해당 봉사활동을 신청하시겠습니까?'
+              : '해당 봉사활동을 삭제하시겠습니까?'
+          }
+          onCancel={closeModal}
+          onConfirm={() => {
+            applyDelete();
+            closeModal();
+          }}
+          confirmText={isApply ? '신청' : '삭제'}
+          confirmColor={isApply ? 'green' : 'pink'}
+        />
+      )}
+    </>
   );
 }
