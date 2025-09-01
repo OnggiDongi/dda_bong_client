caff2f9346a647eff8d41428d24f80d814ed44aa
kimkiyeonn <paynjoy2@naver.com>
2025-09-01 16:33:07 +0900

    🛠️ Refactor: 인증서 배경색 미리 저장

--- ddabong/src/app/home/page.tsx
+++ ddabong/src/app/home/page.tsx
@@ -6,7 +6,7 @@ import ProfileCard from '@/components/home/ProfileCard';
 
 export default function HomePage() {
   return (
-    <main className='flex flex-col items-center gap-4 px-5 pt-5'>
+    <main className='flex flex-col items-center gap-4 px-5 pt-5 pb-10'>
       <Header />
       <ProfileCard username={'별돌이군'} tier={'Silver'} totalHours={72} />
       <ApplyBanner />
--- ddabong/src/components/home/CertificateCard.tsx
+++ ddabong/src/components/home/CertificateCard.tsx
@@ -8,20 +8,35 @@ const LOGO_SRC = '/icons/ic_logo.svg';
 const HANA_SRC = '/icons/ic_hanabank.svg';
 
 type Props = {
-  title: string;
+  userName: string;
   date: string;
-  totalHours: string;
-  bgColor: string; // "bg-1Q-Mint" | "bg-1Q-Purple"
-  borderColor?: string; // "border-1Q-Mint-Line" | "border-1Q-Purple-Line"
+  totalHours: number;
 };
 
-export default function CertificateCard({
-  title,
-  date,
-  totalHours,
-  bgColor,
-  borderColor,
-}: Props) {
+function getCertificateStyle(totalHours: number) {
+  switch (totalHours) {
+    case 50:
+      return { bgColor: 'bg-1Q-Mint', borderColor: 'border-1Q-Mint-Line' };
+    case 100:
+      return { bgColor: 'bg-1Q-Purple', borderColor: 'border-1Q-Purple-Line' };
+    case 150:
+      return {
+        bgColor: 'bg-Hana-Pink',
+        borderColor: 'border-Hana-Pink-border',
+      };
+    case 200:
+      return {
+        bgColor: 'bg-Hana-Beige',
+        borderColor: 'border-Hana-Beige-Line',
+      };
+    default:
+      return { bgColor: 'bg-1Q-Mint', borderColor: 'border-1Q-Mint-Line' };
+  }
+}
+
+export default function CertificateCard({ userName, date, totalHours }: Props) {
+  const { bgColor, borderColor } = getCertificateStyle(totalHours);
+
   return (
     <div
       className={`${bgColor} ${borderColor} relative flex h-[180px] w-[120px] flex-col items-center rounded-2xl pt-6 text-center`}
@@ -38,15 +53,13 @@ export default function CertificateCard({
 
       <Image
         src={ICON_SRC}
-        alt={title}
+        alt='인증서 아이콘'
         width={70}
         height={70}
         className='justify-center'
       />
 
-      <Txt weight='semibold' className='text-Hana-Black pt-0.5 text-[10px]'>
-        {title}
-      </Txt>
+      <Txt className='pt-0.5 text-[10px]'>{userName}</Txt>
       <Txt className='text-Icon-Detail text-[7px]'>{date}</Txt>
       <Txt weight='bold' className='text-Logo-Mint pt-1 text-[11px]'>
         봉사 {totalHours}시간 달성!
