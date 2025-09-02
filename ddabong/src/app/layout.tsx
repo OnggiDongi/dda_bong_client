fa83d8a5a6729f57a98e3980fa54d381c822b5de
dlwlals1289 <dlwlals1289@gmail.com>
2025-09-02 15:52:53 +0900

    🎈 Chore   : Providers 태그 추가

--- ddabong/src/app/layout.tsx
+++ ddabong/src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { ToastProvider } from '@/contexts/toast/ToastProvider';
 import type { Metadata } from 'next';
 import Providers from '../../Providers';
 import './globals.css';
-import { ToastProvider } from '@/contexts/toast/ToastProvider';
 
 export const metadata: Metadata = {
   title: 'DDABONG 따봉',
@@ -18,7 +18,9 @@ export default function RootLayout({
     <html lang='ko'>
       <body className='flex h-screen items-center justify-center overflow-hidden antialiased'>
         <main className='bg-Page-Background h-full w-full overflow-y-auto shadow-[0_0_10px_rgba(0,0,0,0.3)] sm:max-w-sm [&::-webkit-scrollbar]:hidden'>
-          <ToastProvider>{children}</ToastProvider>
+          <Providers>
+            <ToastProvider>{children}</ToastProvider>
+          </Providers>
         </main>
       </body>
     </html>
