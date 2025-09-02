bdbfe3c6a6acbb69703ae452750486f0a29e00f1
dlwlals1289 <dlwlals1289@gmail.com>
2025-09-02 15:19:15 +0900

    📌 Feat    : 기관 로그인 API 연결

--- ddabong/src/app/signIn/page.tsx
+++ ddabong/src/app/signIn/page.tsx
@@ -30,6 +30,8 @@ export default function SeniorSignInPage() {
         }
       );
 
+      console.log(data);
+
       const { accessToken, refreshToken, name, firstLogin } = data ?? {};
 
       localStorage.setItem('accessToken', accessToken ?? '');
