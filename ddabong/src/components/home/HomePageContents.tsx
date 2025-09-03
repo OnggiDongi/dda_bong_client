00471aa660b21949394656d55fa8a2d1069cec8d
Ausdauer1 <steady051@naver.com>
2025-09-03 10:27:10 +0900

    📌 Feat    : 봉사 선호 지역 & 관심카테고리 modal (api 연결)

--- ddabong/src/components/home/HomePageContents.tsx
+++ ddabong/src/components/home/HomePageContents.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useToast } from '@/contexts/toast/ToastContext';
+import axios from 'axios';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
 import OnboardingModal from '../atoms/OnboardingModal';
@@ -9,7 +11,20 @@ import CertificatesSection from './CertificatesSection';
 import Header from './Header';
 import ProfileCard from './ProfileCard';
 
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+const CATEGORY_MAP = {
+  생활: 'LIVING',
+  교육: 'EDUCATION',
+  보건: 'SAFETY',
+  문화: 'CULTURE',
+  환경: 'ENVIRONMENT',
+  행정: 'PUBLIC',
+  농어촌: 'GLOBAL',
+};
+
 export default function HomePageContents() {
+  const { showToast } = useToast();
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -33,8 +48,26 @@ export default function HomePageContents() {
 
   const handleSubmit = async (v: { region: string; interest: string }) => {
     try {
-      console.log(v.region, v.interest);
+      const token = localStorage.getItem('accessToken');
+      await axios.post(
+        baseUrl + '/users/onboarding',
+        {
+          preferredRegion: v.region,
+          preferredCategory: [
+            CATEGORY_MAP[v.interest as keyof typeof CATEGORY_MAP],
+          ],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      showToast('온보딩이 완료되었습니다.');
       localStorage.setItem('seen_onboarding', '1');
+    } catch {
+      showToast('온보딩 실패. 잠시후 다시 시도해주세요');
     } finally {
       setOpen(false);
       router.replace('/home');
