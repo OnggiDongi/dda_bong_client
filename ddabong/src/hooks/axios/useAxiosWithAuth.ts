69a372f1c50123516f5f004409497be4d0a6c0de
Ausdauer1 <steady051@naver.com>
2025-09-05 16:04:56 +0900

    🛠️  Refactor: Axios 훅 변경(accessToken 못받아올때 분기 처리)

--- ddabong/src/hooks/axios/useAxiosWithAuth.ts
+++ ddabong/src/hooks/axios/useAxiosWithAuth.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export function UseAxiosWithAuth() {
-  const token = localStorage.getItem('accessToken');
+  const token =
+    typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
 
   return axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
