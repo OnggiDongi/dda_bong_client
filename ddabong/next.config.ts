5ffd46b72d6c9932109349ccdac8be00d134749d
kimkiyeonn <paynjoy2@naver.com>
2025-09-08 10:37:08 +0900

    🛠️  Refactor: next.config.ts 에 이미지 추가

--- ddabong/next.config.ts
+++ ddabong/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
-  output: 'standalone',
   experimental: {
     useCache: true,
   },
@@ -11,6 +9,24 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         hostname: 'ddabong-upload.s3.ap-northeast-2.amazonaws.com',
+      },
+      {
+        hostname: 'k.kakaocdn.net',
+      },
+      {
+        hostname: 'img1.kakaocdn.net',
+      },
+      {
+        hostname: 'img2.kakaocdn.net',
+      },
+      {
+        hostname: 'img3.kakaocdn.net',
+      },
+      {
+        hostname: 'img4.kakaocdn.net',
+      },
+      {
+        hostname: 'img5.kakaocdn.net',
       },
     ],
   },
